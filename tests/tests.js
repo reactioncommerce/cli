723f51cab475b21e9f6f33c3a67fc49b7891f195
Brent Hoover <brent@thebuddhalodge.com>
2021-12-15 16:42:46 +0800

    (feat) add test scaffolding
    Signed-off-by: Brent Hoover <brent@thebuddhalodge.com>

---
+++ tests/tests.js
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+
+describe("things work", () => {
+  it("for every command", () => {
+    // eslint-disable-next-line jest/valid-expect
+    expect(true).to.equal(true);
+  });
+});
