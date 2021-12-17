fbb6570e372ac1c95ad1e0be67c6d690b0513cf6
Brent Hoover <brent@thebuddhalodge.com>
2021-12-17 16:42:46 +0800

    (feat) Add create-project admin and develop admin commands
    Signed-off-by: Brent Hoover <brent@thebuddhalodge.com>

--- commands/create-project-admin.js
+++ commands/create-project-admin.js
@@ -1,4 +1,7 @@
+import simpleGit from "simple-git";
+import { copy } from "fs-extra";
 import Logger from "../utils/logger.js";
+
 
 /**
  * @summary clones projects locally from repo
@@ -8,4 +11,19 @@ import Logger from "../utils/logger.js";
  */
 export default async function createProjectAdmin(projectName, options) {
   Logger.info({ projectName, options }, "Creating admin");
+  const gitOptions = {
+    baseDir: `${process.cwd()}`,
+    binary: "git",
+    maxConcurrentProcesses: 6
+  };
+  const git = simpleGit(gitOptions);
+  try {
+    await git.clone("https://github.com/reactioncommerce/reaction-admin.git", projectName);
+  } catch (error) {
+    Logger.error(error);
+  }
+  await copy(`${projectName}/.env.example`, `${projectName}/.env`);
+  Logger.info("Admin project created. You can change to this directory and run `npm install`");
+  return true;
 }
+
--- commands/develop-admin.js
+++ commands/develop-admin.js
@@ -1,3 +1,5 @@
+import { spawn } from "child_process";
+import diehard from "diehard";
 import Logger from "../utils/logger.js";
 
 /**
@@ -6,5 +8,20 @@ import Logger from "../utils/logger.js";
  * @returns {Boolean} true for success
  */
 export default async function developAdmin(options) {
-  Logger.info({ options }, "starting development on admin");
+  Logger.info({ options }, "Starting Open Commerce Admin Application Server in dev mode");
+  const api = spawn("npm", ["run", "start:dev"]);
+  api.stdout.on("data", (data) => {
+    // eslint-disable-next-line no-console
+    console.log(data.toString().trim()); // Echo output of command to console
+  });
+
+  diehard.register(async (signal, uncaughtErr, done) => {
+    if (signal === "SIGINT") {
+      Logger.warn("Shutting down from Ctrl-C");
+    }
+    await spawn("docker-compose", ["down"], { PORT: 4080 });
+    done();
+  });
+
+  diehard.listen();
 }
