d195b8faca7cd9a19ca4109430aa8c96b10dc29b
Brent Hoover <brent@thebuddhalodge.com>
2021-12-17 15:20:35 +0800

    (feat) Add develop command
    Signed-off-by: Brent Hoover <brent@thebuddhalodge.com>

--- commands/develop-api.js
+++ commands/develop-api.js
@@ -1,3 +1,5 @@
+import { spawn } from "child_process";
+import diehard from "diehard";
 import Logger from "../utils/logger.js";
 
 /**
@@ -7,4 +9,26 @@ import Logger from "../utils/logger.js";
  */
 export default async function developApi(options) {
   Logger.info({ options }, "starting development on api");
+  Logger.info("Starting Mongo docker image");
+  const mongo = spawn("docker-compose", ["up", "-d"]);
+  mongo.stdout.on("data", (data) => {
+    // eslint-disable-next-line no-console
+    console.log(data.toString().trim()); // Echo output of command to console
+  });
+  Logger.info("Starting Open Commerce API Server in dev mode");
+  const api = spawn("npm", ["run", "start:dev"]);
+  api.stdout.on("data", (data) => {
+    // eslint-disable-next-line no-console
+    console.log(data.toString().trim()); // Echo output of command to console
+  });
+
+  diehard.register(async (signal, uncaughtErr, done) => {
+    if (signal === "SIGINT") {
+      Logger.warn("Shutting down from Ctrl-C");
+    }
+    await spawn("docker-compose", ["down"]);
+    done();
+  });
+
+  diehard.listen();
 }
