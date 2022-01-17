471b353d0369f1b32e69bf457d4484d4a70ee66a
Brent Hoover <brent@thebuddhalodge.com>
2022-01-17 10:19:59 +0800

    feat: changes from c/r
    Signed-off-by: Brent Hoover <brent@thebuddhalodge.com>

--- commands/demo.js
+++ commands/demo.js
@@ -60,5 +60,6 @@ export default async function demo(demoPath) {
   Logger.info("Admin panel on localhost:4080");
   Logger.info("Example Storefront on localhost:4000");
   Logger.info("Run cd <mydemo directory> and then docker-compose down to stop");
+  Logger.info("or use demo-destroy to completely remove a demo");
   return true;
 }
