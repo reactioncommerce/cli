b618eed1127dcb4f0dd46dc0512b4bee086af31a
Brent Hoover <brent@thebuddhalodge.com>
2022-01-28 14:38:32 +0800

    fix: eliminate needless destructuring
    Signed-off-by: Brent Hoover <brent@thebuddhalodge.com>

--- utils/checkForNewVersion.js
+++ utils/checkForNewVersion.js
@@ -2,16 +2,12 @@ import updateNotifier from "update-notifier";
 
 import pkg from "../package.json";
 
-const notifierPackage = {
-  name: pkg.name,
-  version: pkg.version
-};
 
 /**
  * @summary Check if new version is available and notify user if so
  * @returns {Promise<void>} undefined
  */
 export default async function checkForNewVersion() {
-  const notifier = updateNotifier({ pkg: notifierPackage });
+  const notifier = updateNotifier({ pkg });
   notifier.notify();
 }
