9544fc13b262311aaf050bf4bbc99bc69fe5d5cf
Akarshit Wal <akarshitwal@gmail.com>
2022-01-28 13:22:04 -0500

    fix: mocking user for tests
    Signed-off-by: Akarshit Wal <akarshitwal@gmail.com>

--- tests/create-api-project.js
+++ tests/create-api-project.js
@@ -1,11 +1,17 @@
 /* eslint-disable jest/valid-expect */
 import { EOL } from "os";
+import { v4 as uuidv4 } from "uuid";
 import rimraf from "rimraf";
 import { expect } from "chai";
+import getConfig from "../utils/getConfig.js";
 import execute from "./utils/execute.js";
+
+const config = getConfig();
 
 beforeEach(async () => {
   await rimraf.sync("./myshop");
+  // Mock a user is set, to bypass telemetry error complaining no user is set
+  config.set("userId", uuidv4());
 });
 
 describe("the node used for tests", () => {
@@ -23,4 +29,8 @@ describe("The create-project-api command", () => {
     // eslint-disable-next-line jest/valid-expect
     expect(responseLines[1]).to.equal("reaction-cli: Project creation complete. Change to your directory and run `npm install`");
   }).timeout(5000);
+});
+
+afterEach(async () => {
+  config.set("userId", undefined);
 });
