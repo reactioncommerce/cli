b35e066f616572763b9a15efc759fd76a17030ff
Akarshit Wal <akarshitwal@gmail.com>
2022-01-28 13:09:32 -0500

    fix: passing flags while testing, set runOnce true
    Signed-off-by: Akarshit Wal <akarshitwal@gmail.com>

--- tests/create-admin-project.js
+++ tests/create-admin-project.js
@@ -2,21 +2,26 @@
 import { EOL } from "os";
 import rimraf from "rimraf";
 import { expect } from "chai";
+import getConfig from "../utils/getConfig.js";
 import execute from "./utils/execute.js";
+
+const config = getConfig();
 
 beforeEach(async () => {
   await rimraf.sync("./myadmin");
+  // Mock that we have alredy used the command to bypass telemetry logs
+  config.set("runOnce", true);
 });
-
 
 describe("The create-project-admin command", () => {
   it("should print the correct output", async () => {
-    const response = await execute(
-      "./index.js",
-      ["create-project", "admin", "myadmin"]
-    );
+    const response = await execute("./index.js", ["create-project", "admin", "myadmin"]);
     const responseLines = response.trim().split(EOL);
     // eslint-disable-next-line jest/valid-expect
-    expect(responseLines[0]).to.equal("reaction-cli: Creating admin: {\"projectName\":\"myadmin\",\"options\":{}})");
+    expect(responseLines[0]).to.equal('reaction-cli: Creating admin: {"projectName":"myadmin","options":{}})');
   }).timeout(350000); // cloning the admin takes a long time
+});
+
+afterEach(async () => {
+  config.set("runOnce", false);
 });
