3fea816539312025126ebf82e85c0e746aba3ce0
Akarshit Wal <akarshitwal@gmail.com>
2022-03-02 15:24:10 -0500

    fix: adding version check
    Signed-off-by: Akarshit Wal <akarshitwal@gmail.com>

--- commands/index.js
+++ commands/index.js
@@ -6,40 +6,47 @@ import createProject from "./create-project.js";
 import createPlugin from "./create-plugin.js";
 import develop from "./develop.js";
 import telemetry from "./telemetry.js";
+import versionSupportCheck from "./versionSupportCheck.js";
 import build from "./build.js";
 
 export default {
   demo: (demoPath) => {
+    versionSupportCheck();
     telemetryCheck();
     checkForNewVersion();
     createProjectDemo(demoPath);
     track("demo", {}, {});
   },
   createProject: (projectType, projectName, options) => {
+    versionSupportCheck();
     telemetryCheck();
     checkForNewVersion();
     createProject(projectType, projectName, options);
     track(`create-project/${projectType}`, {}, options);
   },
   develop: (projectType, options) => {
+    versionSupportCheck();
     telemetryCheck();
     checkForNewVersion();
     develop(projectType, options);
     track(`develop/${projectType}`, {}, options);
   },
   createPlugin: (type, pluginName, options) => {
+    versionSupportCheck();
     telemetryCheck();
     checkForNewVersion();
     createPlugin(type, pluginName, options);
     track(`create-plugin/${type}`, {}, options);
   },
   build: (type, options) => {
+    versionSupportCheck();
     telemetryCheck();
     checkForNewVersion();
     build(type, options);
     track(`build/${type}`, {}, options);
   },
   telemetry: (args) => {
+    versionSupportCheck();
     telemetryCheck();
     checkForNewVersion();
     telemetry(args);
