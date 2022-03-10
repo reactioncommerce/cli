69622fb3061d9432e9ace89de9e05b9601dfa724
Akarshit Wal <akarshitwal@gmail.com>
2022-03-10 10:20:03 -0500

    fix: doing version compare instead of exact match
    Signed-off-by: Akarshit Wal <akarshitwal@gmail.com>

--- utils/versionSupportCheck.js
+++ utils/versionSupportCheck.js
@@ -1,5 +1,6 @@
 import { createRequire } from "module";
 import fetch from "sync-fetch";
+import compare from "compare-versions";
 import Logger from "../utils/logger.js";
 
 const require = createRequire(import.meta.url);
@@ -17,16 +18,25 @@ export default function versionSupportCheck() {
     const currentVersion = pkg.version;
 
     // Check for any messages for this version
-    const messages = supported?.messages?.[currentVersion] ?? [];
-    for (const msg of messages) {
-      Logger.info(msg);
+    // we are doing a version compare here
+    const messages = supported?.messages ?? {};
+    for (const [key, msgs] of Object.entries(messages)) {
+      if (compare.satisfies(currentVersion, key)) {
+        for (const msg of msgs) {
+          Logger.info(msg);
+        }
+      }
     }
 
+    const unsupportedVersions = supported?.unsupportedVersions ?? [];
+
     // Check for unsupported versions
-    if (supported?.unsupportedVersions?.indexOf(currentVersion) > -1) {
-      // The current version is not supported. Exit!
-      Logger.error("This version is no longer supported. Please update reaction cli using\n\t npm update -g @reactioncommerce/reaction-cli");
-      process.exit(); // eslint-disable-line no-process-exit
+    for (const version of unsupportedVersions) {
+      if (compare.satisfies(currentVersion, version)) {
+        // The current version is not supported. Exit!
+        Logger.error("This version is no longer supported. Please update reaction cli using\n\t npm install -g @reactioncommerce/reaction-cli");
+        process.exit(); // eslint-disable-line no-process-exit
+      }
     }
   } catch (err) {
     // Swallowing the error if we can't fetch supported versions
