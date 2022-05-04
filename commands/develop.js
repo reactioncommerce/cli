7423fe01fe642b2d2e2dc5c6ccbb70af6d566aa6
Sujith <sujith@merchstack.com>
2022-05-04 09:37:24 +0530

    feat-sujith-autoDetect-projectType
    Signed-off-by: Sujith <sujith@merchstack.com>

--- commands/develop.js
+++ commands/develop.js
@@ -1,7 +1,9 @@
+import fs from "fs";
 import checkDependencies from "../utils/checkDependencies.js";
 import developStorefront from "./develop-storefront.js";
 import developAdmin from "./develop-admin.js";
 import developApi from "./develop-api.js";
+import Logger from "../utils/logger.js";
 
 const functionMap = {
   api: developApi,
@@ -13,6 +15,43 @@ const extraDependencyMap = {
   storefront: ["yarn"]
 };
 
+const validProjectTypes = ['api', 'admin-meteor', 'storefront-example'];
+
+/**
+ * @summary check if reactionProjectType exists, if not return empty string
+ * @returns {String} - The project type
+ */
+ async function getProjectType() {
+  Logger.info("Getting project type");
+  const packageJson = fs.readFileSync("package.json", { encoding: "utf8", flag: "r" });
+  const packageJsonData = JSON.parse(packageJson);
+  const projectType = packageJsonData.reactionProjectType;
+
+  if (!projectType || projectType === "") {
+    Logger.error("No project type found in package.json");
+    return "";
+  }
+
+  if (!validProjectTypes.includes(projectType)) {
+    Logger.error("No valid project type found in package.json");
+    return "";
+  }
+
+  switch (projectType) {
+    case "api":
+      Logger.info("Found project type: api");
+      return "api";
+    case "admin-meteor":
+      Logger.info("Found project type: admin-meteor");
+      return "admin";
+    case "storefront-example":
+      Logger.info("Found project type: storefront-example");
+      return "storefront";
+    default:
+      Logger.error("No valid project type found in package.json");
+      return "";
+  }
+ }
 
 /**
  * @summary Run api in development mode
@@ -21,9 +60,21 @@ const extraDependencyMap = {
  * @returns {Promise<Boolean>} True for success
  */
 export default async function develop(projectType, options) {
-  const dependenciesOk = await checkDependencies(extraDependencyMap[projectType]);
+  Logger.info("Developing project", { projectType, options });
+  let projectTypeFound = "";
+  if (projectType) {
+    projectTypeFound = projectType;
+  } else {
+    projectTypeFound = await getProjectType();
+  }
+  if (!projectTypeFound || projectTypeFound === "") {
+    Logger.error("No project type found");
+    return false;
+  }
+
+  const dependenciesOk = await checkDependencies(extraDependencyMap[projectTypeFound]);
   if (dependenciesOk) {
-    return functionMap[projectType](options);
+    return functionMap[projectTypeFound](options);
   }
   return false;
 }
