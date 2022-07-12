d7ca815a6b71ae5e469f2e5397d24884c4b09602
Brian Nguyen <vanpho02@gmail.com>
2022-07-12 08:38:41 +0700

    fix: install pnpm if not exists
    Signed-off-by: Brian Nguyen <vanpho02@gmail.com>

--- tests/create-kinetic-project.js
+++ tests/create-kinetic-project.js
@@ -1,7 +1,9 @@
 /* eslint-disable jest/valid-expect */
 import { EOL } from "os";
+import { spawn } from "child_process";
 import rimraf from "rimraf";
 import { expect } from "chai";
+import { sync as cmdExists } from "command-exists";
 import getConfig from "../utils/getConfig.js";
 import execute from "./utils/execute.js";
 
@@ -11,6 +13,9 @@ beforeEach(async () => {
   await rimraf.sync("./mykinetic");
   // Mock that we have alredy used the command to bypass telemetry logs
   config.set("runOnce", true);
+  if (!cmdExists("pnpm")) {
+    spawn("npm", ["install", "pnpm", "-g"]);
+  }
 });
 
 describe("The create-project-kinetic command", () => {
@@ -18,7 +23,7 @@ describe("The create-project-kinetic command", () => {
     const response = await execute("./index.js", ["create-project", "kinetic", "mykinetic"]);
     const responseLines = response.trim().split(EOL);
     // eslint-disable-next-line jest/valid-expect
-    expect(responseLines[0]).to.equal('reaction-cli: Creating kinetic: {"projectName":"mykinetic","options":{}}');
+    expect(responseLines[0]).to.equal('reaction-cli: Creating kinetic: {"projectName":"mykinetic"}');
   }).timeout(350000); // cloning the admin takes a long time
 });
 
